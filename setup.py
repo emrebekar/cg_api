d1ede99c305e11584056c3cf7439432fd6af8227
Emre Bekar <emrebekar67@gmail.com>
2022-01-31 22:38:41 +0300

    version update to 1.0.1

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
 setuptools.setup(
-    name='coingeckoapi',
-    version='1.0.0',
-    packages=['coingeckoapi',],
+    name='cg_api',
+    version='1.0.1',
+    packages=['src',],
     license='MIT',
     description = 'Python project for CoingeckoAPI',
     long_description=open('README.md').read(),
@@ -11,7 +11,8 @@ setuptools.setup(
     author = 'Emre Bekar',
     author_email = 'emre.bekar@hotmail.com.tr',
     install_requires=['requests'],
-    url = 'https://github.com/emrebekar/coingeckoapi',
+    url = 'https://github.com/emrebekar/cg_api',
+    download_url = 'https://github.com/emrebekar/coingeckoapi/archive/cg_api-1.0.1.tar.gz',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
